a1751c4733af046bf13006d4a1e39060b9755408
Hadi Qovaizi <hadi.qovaizi@gmail.com>
2020-05-31 11:11:46 -0400

    Add "Hello World!" Java solution

---
+++ Java/hello.java
@@ -0,0 +1,12 @@
+/*
+ *	Problem: Hello World!
+ */
+
+public class hello {
+
+	/*-----main-----*/
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	} // end main
+	
+} // end class
