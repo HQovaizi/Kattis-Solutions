31b717c19e035f5a85ff354ec3cf1a98058dc6c1
Hadi Qovaizi <hadi.qovaizi@gmail.com>
2020-06-24 14:01:46 -0400

    Add "A Different Problem" C++ solution

---
+++ C++/different.cpp
@@ -0,0 +1,18 @@
+/*
+ *	Problem: "A Different Problem"
+ */
+ 
+#include <iostream>
+
+using namespace std;
+
+/*-----main-----*/
+int main(void) {
+	long long a, b;
+
+	while (cin >> a >> b) {
+		cout << abs(a - b) << endl;
+	}
+
+	return 0;
+}
