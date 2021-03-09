bfa12b5b481d70531f10d9235031ff3188cdbfd0
Hadi Qovaizi <hadi.qovaizi@gmail.com>
2021-03-09 09:56:28 -0500

    Add "Beavergnaw" Python3 solution

---
+++ Python/beavergnaw.py
@@ -0,0 +1,19 @@
+'''
+ *
+ *	Problem: "Beavergnaw"
+ *
+'''
+
+import math
+
+D, V = map(int, input().split(" "))
+
+while ( ((D) and (V)) != 0 ):
+	v = (12*(D*((D/2)**2) - V/math.pi) - (D**3))/2
+	d = v ** (1. / 3)
+	
+	print( "%.9f\n" % (d) )
+	
+	d
+	D, V = map(int, input().split(" "))
+	
