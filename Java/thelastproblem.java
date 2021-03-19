008f984e48707cc72178d0c84a92f77fbfeab549
Hadi Qovaizi <hadi.qovaizi@gmail.com>
2021-03-19 13:26:28 -0400

    Add "The Last Problem" Java solution

---
+++ Java/thelastproblem.java
@@ -0,0 +1,16 @@
+/*
+ *	Problem: "The Last Problem"
+ */
+
+import java.util.Scanner;
+
+public class thelastproblem {
+		
+	/*-----main-----*/
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		System.out.println("Thank you, " + name + ", and farewell!");
+	}
+	
+}
