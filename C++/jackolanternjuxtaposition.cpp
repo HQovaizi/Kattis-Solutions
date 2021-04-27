59eb2312103f93fbd559b481e90db102221f2b9c
Hadi Qovaizi <hadi.qovaizi@gmail.com>
2021-04-27 00:16:13 -0400

    Add "Jack-O"-Lantern Juxtaposition" C++ solution

---
+++ C++/jackolanternjuxtaposition.cpp
@@ -0,0 +1,19 @@
+/*
+ *	Problem: "Jack-O'-Lantern Juxtaposition"
+ */
+ 
+#include <iostream>
+
+using namespace std;
+
+/*-----main-----*/
+int main(void) {
+
+	int numEyes, numNose, numMouth;
+  cin >> numEyes >> numNose >> numMouth;
+	
+  int result = numEyes * numNose * numMouth;
+  cout << result;
+	
+  return 0;
+} // end main
