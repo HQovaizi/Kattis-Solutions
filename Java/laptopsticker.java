af231ff5d945843ccd50371655b7d519fbc5c784
Hadi Qovaizi <hadi.qovaizi@gmail.com>
2021-05-04 00:20:15 -0400

    Add "Laptop Sticker" Java solution

---
+++ Java/laptopsticker.java
@@ -0,0 +1,38 @@
+/*
+ *	Problem: 		Laptop Sticker
+ *	Problem ID:	laptopsticker
+ *	url:				https://open.kattis.com/problems/laptopsticker
+ *	Solution: 	Java
+ */
+
+import java.util.Scanner;
+
+public class laptopsticker {
+
+	/*-----result-----*/
+	public static void result() {
+		Scanner sc = new Scanner(System.in);
+		int widthComputer, heightComputer, widthSticker, heightSticker;
+		boolean widthExceeded, heightExceeded;
+		
+		widthComputer = sc.nextInt();
+		heightComputer = sc.nextInt();
+		widthSticker = sc.nextInt();
+		heightSticker = sc.nextInt();
+		sc.close();
+		
+		widthExceeded = (widthSticker >= (widthComputer - 1));
+		heightExceeded = (heightSticker >= (heightComputer - 1));
+		
+		if ( (!widthExceeded) && (!heightExceeded) )
+			System.out.println(1);
+		else
+			System.out.println(0);
+	} // end result
+	
+	/*-----main-----*/
+	public static void main(String[] args) {
+		result();
+	} // end main
+
+} // end class
