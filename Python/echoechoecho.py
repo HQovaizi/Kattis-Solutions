4f30406207da37c8f26ac2b681542519864706b5
Hadi Qovaizi <hadi.qovaizi@gmail.com>
2021-07-08 14:14:33 -0400

    Add "Echo Echo Echo" Python solution

---
+++ Python/echoechoecho.py
@@ -0,0 +1,15 @@
+'''
+ *
+ *	Problem:		Echo Echo Echo
+ *	Problem ID:	echoechoecho
+ *	url:				https://open.kattis.com/problems/echoechoecho
+ *	Solution:		Python
+ *
+'''
+
+NUMBER_REPITIONS = 3
+
+line = input()
+output = (line + " ") * NUMBER_REPITIONS
+
+print(output)
