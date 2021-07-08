d2523dc4113afbd6a7fdac6aa08a344093e03c69
Hadi Qovaizi <hadi.qovaizi@gmail.com>
2021-07-08 14:12:44 -0400

    Add "Add Two Numbers" Python solution

---
+++ Python/addtwonumbers.py
@@ -0,0 +1,12 @@
+'''
+ *
+ *	Problem:		Add Two Numbers
+ *	Problem ID:	addtwonumbers
+ *	url:				https://open.kattis.com/problems/addtwonumbers
+ *	Solution:		Python
+ *
+'''
+
+operand_a, operand_b  = map(int, input().split(" "))
+sum = operand_a + operand_b
+print(sum)
